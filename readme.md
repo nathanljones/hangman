dfa86ee9d9ef85b08300107e40e9bed3d5f37b76
nathanljones <nathan.l.jones+github@protonmail.com>
2023-08-28 11:38:06 +0100

    completed game model

--- readme.md
+++ readme.md
@@ -1,3 +1,18 @@
 # Hangman Game In Rust
 
-Learning Rust via creating a simple hangman game
+## Learning Rust via creating a simple hangman game
+
+### MVP
+
+1. Pick word from a hard coded list
+2. Get user input
+3. Check if letter is in word
+4. Decrement lives if not
+5. Update guess with letters found
+6. Check if lives used up
+7. Check if winning
+
+### Future Enhancements
+- [ ] Read words in from a file
+- [ ] Terminal display of hangman
+- [ ] Output using a graphics engine
--- src/game.rs
+++ src/game.rs
@@ -6,6 +6,16 @@ struct GameStore {
     current_word: String,
 }
 
+#[derive(PartialEq, Debug)]
+enum LetterInWord {
+    Found,
+    NotFound,
+}
+#[derive(PartialEq, Debug)]
+enum MatchingWord {
+    Matched,
+    NotMatched,
+}
 impl GameStore {
     fn new() -> Self {
         GameStore {
@@ -13,6 +23,38 @@ impl GameStore {
             lives_left: 0,
             letters_used: vec![],
             current_word: String::from(""),
+        }
+    }
+    fn set_word_to_find(&mut self, secret_word: &str) {
+        self.word_to_find = secret_word.to_owned();
+        self.current_word = self.word_to_find.chars().map(|_c| "_").collect();
+    }
+    fn check_for_letter_in_word(self, letter: &str) -> LetterInWord {
+        match self.word_to_find.find(letter) {
+            Some(_) => LetterInWord::Found,
+            None => LetterInWord::NotFound,
+        }
+    }
+    fn add_letter_to_letters_used(&mut self, letter_to_add: &str) {
+        if !self.letters_used.iter().any(|i| i == "letter_to_add") {
+            self.letters_used.push(letter_to_add.to_owned());
+        };
+    }
+    fn decrement_lives_left(&mut self) {
+        self.lives_left -= 1;
+    }
+    fn check_word_is_found(self) -> MatchingWord {
+        if self.word_to_find == self.current_word {
+            MatchingWord::Matched
+        } else {
+            MatchingWord::NotMatched
+        }
+    }
+    fn update_current_word_with_letter(&mut self, letter_to_update: &str) {
+        for (i, c) in self.word_to_find.chars().enumerate() {
+            if c.to_string() == letter_to_update {
+                self.current_word.replace_range(i..i + 1, letter_to_update)
+            }
         }
     }
 }
@@ -26,4 +68,71 @@ fn generate_word_to_guess() -> String {
 pub fn game_run() {
     let mut current_game_store = GameStore::new();
     current_game_store.word_to_find = generate_word_to_guess();
+}
+
+#[test]
+fn secret_word_masks() {
+    let mut current_game_store = GameStore::new();
+    current_game_store.set_word_to_find("secret");
+    assert_eq!(current_game_store.current_word, "______");
+}
+
+#[test]
+fn letter_is_in_word() {
+    let mut current_game_store = GameStore::new();
+    current_game_store.set_word_to_find("secret");
+    assert_eq!(
+        current_game_store.check_for_letter_in_word("s"),
+        LetterInWord::Found,
+    );
+}
+#[test]
+fn letter_is_not_in_word() {
+    let mut current_game_store = GameStore::new();
+    current_game_store.set_word_to_find("secret");
+    assert_eq!(
+        current_game_store.check_for_letter_in_word("q"),
+        LetterInWord::NotFound,
+    );
+}
+#[test]
+fn letter_added_to_letters_used() {
+    let mut current_game_store = GameStore::new();
+    current_game_store.add_letter_to_letters_used("f");
+    assert!(current_game_store.letters_used.iter().any(|i| i == "f"));
+}
+#[test]
+fn lives_can_be_lost() {
+    let mut current_game_store = GameStore::new();
+    current_game_store.lives_left = 30;
+    current_game_store.decrement_lives_left();
+    assert_eq!(current_game_store.lives_left, 29);
+}
+#[test]
+fn matching_word_is_found() {
+    let mut current_game_store = GameStore::new();
+    current_game_store.set_word_to_find("secret");
+    current_game_store.current_word = "secret".to_string();
+    assert_eq!(
+        current_game_store.check_word_is_found(),
+        MatchingWord::Matched
+    );
+}
+#[test]
+fn matching_word_is_not_found() {
+    let mut current_game_store = GameStore::new();
+    current_game_store.set_word_to_find("secret");
+    current_game_store.current_word = "fred".to_string();
+    assert_eq!(
+        current_game_store.check_word_is_found(),
+        MatchingWord::NotMatched
+    );
+}
+
+#[test]
+fn current_word_updates_with_letter() {
+    let mut current_game_store = GameStore::new();
+    current_game_store.set_word_to_find("secret");
+    current_game_store.update_current_word_with_letter("e");
+    assert_eq!(current_game_store.current_word, "_e__e_");
 }
